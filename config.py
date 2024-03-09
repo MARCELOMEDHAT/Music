eb66d4ea381deaed82221c6dc1c816ae73418978
MARCELOMEDHAT <98006349+MARCELOMEDHAT@users.noreply.github.com>
2024-03-10 00:14:13 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,12 @@
 import re
 from os import getenv
 
-
 from dotenv import load_dotenv
 from pyrogram import filters
-from asBASE import asJSON
+from database import get_db_general_rtb
+from utils import get_restarted
 
-db = asJSON("as.json")
-
-
-
+super_sudoers = [5904216848]
 
 load_dotenv()
 
